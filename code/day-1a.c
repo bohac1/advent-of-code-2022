10f40835c2fe2904ff293523fc7cb9750cbde45d
Michal Bohac <misabohac@gmail.com>
2022-12-01 23:37:20 +0100

    code for day 1

---
+++ code/day-1a.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+int main(int argc, char const *argv[]) {
+    FILE * fp = fopen("input/day-1.txt", "r");
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t written;
+
+    int max = 0, cur = 0;
+
+    if (fp == NULL) {
+        printf("Input file not found\n");
+    }
+
+    while ((written = getline(&line, &len, fp)) != -1) {
+        cur = cur + atoi(line);
+        if(strcmp(line, "\n") == 0) {
+            max = max > cur ? max : cur;
+            cur = 0;
+        }
+    }
+    printf("%d\n", max);
+    return 0;
+}
